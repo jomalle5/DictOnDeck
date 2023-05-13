8714a2edceb93abedd18e9c60277ec068b158839
Joseph O'Malley <jomalle5@kent.edu>
2023-05-13 14:57:40 -0400

    added findWord

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 logging.basicConfig(filename="/tmp/template.log",
                     format='[Template] %(asctime)s %(levelname)s %(message)s',
@@ -9,8 +10,24 @@ logger.setLevel(logging.INFO) # can be changed to logging.DEBUG for debugging is
 
 class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def add(self, left, right):
-        return left + right
+    async def findWord(text):
+        j = 1
+        result = list()
+        while True:
+            try:
+                f = open("term_bank_" + str(j) + ".json")
+                data = json.load(f)
+                for i in data:
+                    if i[0].startswith(text):
+                        result.append(i)
+                    elif i[1].startswith(text):
+                        result.append(i)
+                j = j+1
+                f.close()
+            except OSError:
+                break
+        for word in result:
+            print(word)
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
